052204699a013e0092f6a82a9f29ffffb25a26ff
santosh <gsantoshb@gmail.com>
2023-02-26 15:15:36 -0500

    changes and using bootstrap

---
+++ src/LoginPage.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import "./LoginPage.css";
+import { useNavigate } from "react-router-dom";
+import { auth } from "./firebase";
+
+function LoginPage() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        navigate.push("/");
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        // it successfully created a new user with email and password
+        if (auth) {
+          navigate.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  return (
+    <main className="form-signin w-100 m-auto">
+      <form>
+        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+        <div className="form-floating">
+          <input
+            type="email"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label htmlFor="floatingInput">Email address</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label htmlFor="floatingPassword">Password</label>
+        </div>
+
+        <div className="checkbox mb-3">
+          <label>
+            <input type="checkbox" value="remember-me" /> Remember me
+          </label>
+        </div>
+        <button className="w-100 btn btn-lg btn-primary" type="submit">
+          Sign in
+        </button>
+        <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+      </form>
+    </main>
+  );
+}
+
+export default LoginPage;
